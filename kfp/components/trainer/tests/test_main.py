c8ae7078bcd9abca7de5da917d9f176631d631fe
Asei Sugiyama <asei.sugiyama@repro.io>
2021-06-02 15:22:16 +0900

    Update test

--- kfp/components/trainer/tests/test_main.py
+++ kfp/components/trainer/tests/test_main.py
@@ -7,12 +7,13 @@ import pytest
 
 
 class TestFetchDataset:
-    def test_fetch_dataset_from_right_formatted_data(self):
+    def test_load_dataset_from_right_formatted_data(self):
         source = StringIO(
             "species,culmen_length_mm,culmen_depth_mm,flipper_length_mm,body_mass_g\n"
             "0,0.2545454545454545,0.6666666666666666,0.15254237288135594,0.2916666666666667"
         )
-        actual = trainer.load_dataset(source)
+        label_key = "species"
+        actual = trainer.load_dataset(source, label_key)
         assert actual.dtype.names == (
             "species",
             "culmen_length_mm",
@@ -25,27 +26,6 @@ class TestFetchDataset:
 
     def test_fetch_dataset_fails_file_without_header(self):
         source = StringIO("0,1\n" "0,1")
+        label_key = "species"
         with pytest.raises(IndexError):
-            trainer.load_dataset(source)
-
-
-class TestTransformDataset:
-    def test_transform_dataset_from_right_formated_data(self):
-        source = [(0, 0.1, 0.2, 0.3, 0.4)]
-        names = [
-            "species",
-            "culmen_length_mm",
-            "culmen_depth_mm",
-            "flipper_length_mm",
-            "body_mass_g",
-        ]
-        formats = ["i8", "f8", "f8", "f8", "f8"]
-        dtypes = list(zip(names, formats))
-        data = np.array(source, dtype=dtypes)
-        
-        expected_names = [name + "_xf" for name in names]
-        expected_dtypes = list(zip(expected_names, formats))
-        expected = np.array(source, dtype=expected_dtypes)
-        
-        actual = trainer.transform(data, "_xf")
-        assert_equal(actual, expected)
+            trainer.load_dataset(source, label_key)
